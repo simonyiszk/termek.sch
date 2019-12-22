e9498bb12405b31157d565c8a488d40d31ab79b9
Freyb <balazs.frey@gmail.com>
2019-12-22 23:47:23 +0100

    Install Material and FontAwesome icons

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,21 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib';
+import "material-design-icons-iconfont/dist/material-design-icons.css";
+import "@fortawesome/fontawesome-free/css/all.css";
+import Vue from "vue";
+import Vuetify from "vuetify/lib";
+
+import colors from "vuetify/lib/util/colors";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: colors.red.darken1, // #E53935
+        secondary: colors.red.lighten4, // #FFCDD2
+        accent: colors.indigo.base, // #3F51B5
+        simonyi: "#63bc47"
+      }
+    }
+  }
 });
