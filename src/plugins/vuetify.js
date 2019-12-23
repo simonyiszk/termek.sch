b19cf450306633d4ef4ddfd831a82b29d51bc5e4
Freyb <balazs.frey@gmail.com>
2019-12-23 16:03:54 +0100

    Add gray color

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -14,7 +14,8 @@ export default new Vuetify({
         primary: colors.red.darken1, // #E53935
         secondary: colors.red.lighten4, // #FFCDD2
         accent: colors.indigo.base, // #3F51B5
-        simonyi: "#63bc47"
+        simonyi: "#63bc47",
+        gray: "#3D3D3D"
       }
     }
   }
