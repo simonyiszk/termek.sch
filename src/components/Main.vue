8b0fb7f132a219c6c88c2a178fed7a2929ae584e
Freyb <balazs.frey@gmail.com>
2019-12-24 01:04:33 +0100

    Implement Peoplelist component

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -16,13 +16,75 @@
         </v-row>
       </v-container>
     </div>
-    <div></div>
+    <div class="d-flex justify-center">
+        <Peoplelist title="Teremfelelősök" :people="teremfelelos" />
+        <Peoplelist title="Elnökség" :people="elnokseg" />
+    </div>
   </div>
 </template>
 
 <script>
+import Peoplelist from "./Peoplelist";
+
 export default {
-  name: "Main"
+  name: "Main",
+
+  components: { Peoplelist },
+
+  data: () => ({
+    elnokseg: [
+      {
+        name: "Frey Balázs",
+        email: "frey.balazs@simonyi.bme.hu",
+        phone: "+36 70 210 0880",
+        room: "SCH1313"
+      },
+      {
+        name: "Bozsik Szabolcs",
+        email: "bozsik.szabolcs@simonyi.bme.hu",
+        phone: "+36 20 384 2721",
+        room: "SCH1312"
+      },
+      {
+        name: "Kiss Ádám",
+        email: "kiss.adam@simonyi.bme.hu",
+        phone: "+36 30 731 0629",
+        room: "SCH 1306"
+      },
+      {
+        name: "Vankó Dániel",
+        email: "vanko.daniel@simonyi.bme.hu",
+        phone: "+36 70 590 9950",
+        room: "SCH 1316"
+      }
+    ],
+    teremfelelos: [
+      {
+        name: "Bélteky Zsuzsanna",
+        email: "sushi@simonyi.bme.hu",
+        phone: "+36 20 331 3007",
+        room: "SCH 1310"
+      },
+      {
+        name: "Hajas Márk",
+        email: "mark.hajas@gmail.com",
+        phone: "+36 30 480 4616",
+        room: "SCH 816"
+      },
+      {
+        name: "Südi Beáta",
+        email: "bea@simonyi.bme.hu",
+        phone: "+36 30 692 0223",
+        room: "SCH 1311"
+      },
+      {
+        name: "Várszegi Júlia",
+        email: "varszegi.julia@gmail.com",
+        phone: "+36 30 710 7494",
+        room: "SCH 1311"
+      }
+    ]
+  })
 };
 </script>
 
---
+++ src/components/Peoplelist.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-card dark class="peoplelist-container mx-10" color="simonyi">
+    <div class="peoplelist-title">{{title}}</div>
+    <div v-for="p in people" :key="p.name" class="peoplelist-box">
+      <div v-if="p.name">{{p.name}}</div>
+      <div v-if="p.email">{{p.email}}</div>
+      <div v-if="p.phone">{{p.phone}}</div>
+      <div v-if="p.room">{{p.room}}</div>
+    </div>
+  </v-card>
+</template>
+<script>
+import VueTypes from "vue-types";
+
+const Person = VueTypes.shape({
+  name: VueTypes.string.isRequired,
+  email: VueTypes.string,
+  phone: VueTypes.string,
+  room: VueTypes.string
+});
+
+export default {
+  name: "Peoplelist",
+  props: {
+    title: VueTypes.string.isRequired,
+    people: VueTypes.arrayOf(Person)
+  }
+};
+</script>
+<style scoped>
+.peoplelist-container {
+  padding: 1rem;
+  flex-basis: 25%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.peoplelist-title {
+  font-size: 1.5rem;
+}
+.peoplelist-box {
+  width: 100%;
+}
+</style>
